96683f9c896af1aa24c7ece1a28676b316852cdd
ymcx <89810988+ymcx@users.noreply.github.com>
2026-04-10 11:27:58 +0300

    fix unique-binary-search-trees-ii

--- unique-binary-search-trees-ii/main.cpp
+++ unique-binary-search-trees-ii/main.cpp
@@ -1,5 +1,4 @@
-#include <cassert>
-#include <deque>
+#include <map>
 #include <vector>
 
 using namespace std;
@@ -14,67 +13,38 @@ struct TreeNode {
       : val(x), left(left), right(right) {}
 };
 
-vector<TreeNode *> createTree(vector<vector<int>> items_all) {
-  vector<TreeNode *> trees;
+typedef map<pair<int, int>, vector<TreeNode *>> Memo;
 
-  for (const vector<int> &items : items_all) {
-    TreeNode *child_l = nullptr;
-    TreeNode *child_r = nullptr;
-
-    for (int i = items.size() - 1; 0 <= i; i -= 2) {
-      const int &value_l = items[i - 1];
-      const int &value_r = items[i];
-
-      child_l =
-          value_l != -1 ? new TreeNode(value_l, child_l, child_r) : nullptr;
-      child_r =
-          value_r != -1 ? new TreeNode(value_r, child_r, child_l) : nullptr;
-    }
-
-    trees.push_back(child_l);
+// https://leetcode.com/problems/unique-binary-search-trees-ii/editorial/
+vector<TreeNode *> allPossibleBST(const int start, const int end, Memo &memo) {
+  if (start > end) {
+    return {nullptr};
   }
 
-  return trees;
-}
+  if (memo.find({start, end}) != memo.end()) {
+    return memo[{start, end}];
+  }
 
-vector<TreeNode *> generateTrees(int n) {}
+  vector<TreeNode *> result;
+  for (int i = start; i <= end; ++i) {
+    const vector<TreeNode *> leftTrees = allPossibleBST(start, i - 1, memo);
+    const vector<TreeNode *> rightTrees = allPossibleBST(i + 1, end, memo);
 
-bool equals(vector<TreeNode *> a, vector<TreeNode *> b) {
-  for (size_t i = 0; i < a.size(); ++i) {
-    TreeNode *node_a = a[i];
-    TreeNode *node_b = b[i];
-
-    tuple<TreeNode *, TreeNode *> tuplee = {node_a, node_b};
-    deque<tuple<TreeNode *, TreeNode *>> queue = {tuplee};
-    while (!queue.empty()) {
-      auto [na, nb] = queue.back();
-      queue.pop_back();
-
-      if (na == nullptr && nb == nullptr) {
-        continue;
+    for (TreeNode *left : leftTrees) {
+      for (TreeNode *right : rightTrees) {
+        TreeNode *root = new TreeNode(i, left, right);
+        result.push_back(root);
       }
-
-      if (na == nullptr xor nb == nullptr) {
-        return false;
-      }
-
-      if (na->val != nb->val) {
-        return false;
-      }
-
-      queue.emplace_back(na->left, nb->left);
-      queue.emplace_back(na->right, nb->right);
     }
   }
 
-  return true;
+  memo[{start, end}] = result;
+  return result;
 }
 
-int main() {
-  vector<vector<int>> tree = {{{1, -1, 2, -1, 3, -1},
-                               {1, -1, 3, 2},
-                               {2, 1, 3, -1},
-                               {3, 1, -1, -1, 2, -1},
-                               {3, 2, -1, 1}}};
-  assert(equals(generateTrees(3), createTree(tree)));
+vector<TreeNode *> generateTrees(int n) {
+  Memo memo;
+  return allPossibleBST(1, n, memo);
 }
+
+int main() {}
